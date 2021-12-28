63716033f93f38e94ef7679cb97832e6b7e48400
Wagyourtail <wagyourtail@wagyourtail.xyz>
2021-12-27 22:30:47 -0700

    fix

--- common/src/main/java/xyz/wagyourtail/jsmacros/client/api/classes/Inventory.java
+++ common/src/main/java/xyz/wagyourtail/jsmacros/client/api/classes/Inventory.java
@@ -254,7 +254,7 @@ public class Inventory<T extends ContainerScreen<?>> {
             if (hotbarSlot < 0 || hotbarSlot > 8)
                 throw new IllegalArgumentException("hotbarSlot must be between 0 and 8 or 40 for offhand.");
         }
-        man.clickSlot(syncId, slot, hotbarSlot, SlotActionType.SWAP, player);
+        man.method_2906(syncId, slot, hotbarSlot, SlotActionType.SWAP, player);
         return this;
     }
     
